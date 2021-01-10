287cf645e4d4e9b4233a871d5863f0ace8d138b7
Iulian Vărzaru <iulian@varzaru.net>
2021-01-10 15:44:44 +0200

    added workman layout

--- keyboards/crkbd/keymaps/iulian/config.h
+++ keyboards/crkbd/keymaps/iulian/config.h
@@ -17,10 +17,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 #pragma once
-
-//#define USE_MATRIX_I2C
 
 /* Select hand configuration */
 
@@ -28,19 +25,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-#define USE_SERIAL_PD2
+#define USE_SERIAL
 
-#define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
+#define OLED_DISABLE_TIMEOUT
 
-#ifdef RGBLIGHT_ENABLE
-    #undef RGBLED_NUM
-    // #define RGBLIGHT_ANIMATIONS
-    #define RGBLED_NUM 27
-    #define RGBLIGHT_LIMIT_VAL 120
-    #define RGBLIGHT_HUE_STEP 10
-    #define RGBLIGHT_SAT_STEP 17
-    #define RGBLIGHT_VAL_STEP 17
-#endif
+#undef RGBLED_NUM
+// #define RGBLIGHT_ANIMATIONS
+// #define RGBLIGHT_SPLIT
+#define RGBLED_NUM 27
+#define RGBLIGHT_LIMIT_VAL 120
+#define RGBLIGHT_HUE_STEP 10
+#define RGBLIGHT_SAT_STEP 17
+#define RGBLIGHT_VAL_STEP 17
 
 #define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
---
+++ keyboards/crkbd/keymaps/iulian/rules.mk
@@ -0,0 +1,14 @@
+BOOTMAGIC_ENABLE        = no
+MOUSEKEY_ENABLE         = no
+CONSOLE_ENABLE          = no
+COMMAND_ENABLE          = no
+
+RGBLIGHT_ENABLE         = no
+RGB_MATRIX_ENABLE       = no
+BACKLIGHT_ENABLE        = no
+
+EXTRAKEY_ENABLE         = yes
+OLED_DRIVER_ENABLE      = yes
+
+# BOOTLOADER              = atmel-dfu
+SPLIT_TRANSPORT         = mirror
